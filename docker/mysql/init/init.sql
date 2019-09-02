590be10979cb7663a34a7aaaf6110e278b7f24b5
duanshutao <duanshutao@want-want.com>
2019-09-03 03:24:25 +0800

    init

---
+++ docker/mysql/init/init.sql
@@ -0,0 +1,27 @@
+CREATE USER 'luna'@'%' IDENTIFIED BY 'pwdluna';
+GRANT All privileges ON *.* TO 'luna'@'%';
+flush privileges;
+
+
+
+-- GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'example' WITH GRANT OPTION
+-- flush privileges;
+
+
+-- root@localhost init # cat init.sql
+-- use mysql;
+-- ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'example';
+--
+-- create database test;
+-- use test;
+-- create table user
+-- (
+--     id int auto_increment primary key,
+--     username varchar(64) unique not null,
+--     email varchar(120) unique not null,
+--     password_hash varchar(128) not null,
+--     avatar varchar(128) not null
+-- );
+-- insert into user values(1, "zhangsan","test12345@qq.com","passwd","avaterpath");
+-- insert into user values(2, "lisi","12345test@qq.com","passwd","avaterpath");
+--
